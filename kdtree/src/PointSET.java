7f6d5f0a25d4e21ae8fb7afdd09a1b7b574ace7c
thankhoibg <24020185@vnu.edu.vn>
2026-03-20 10:59:01 +0700

    Create dummy file for KDTree Implemention

---
+++ kdtree/src/PointSET.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+// Nếu dùng TreeSet cho PointSET
+import java.util.TreeSet;
+public class PointSET {
+    private TreeSet<Point2D> points;
+    
+}
