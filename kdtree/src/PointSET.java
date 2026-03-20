3f46a498e4b5346e73667deccdf865d1065cf118
thankhoibg <24020185@vnu.edu.vn>
2026-03-20 11:03:53 +0700

    Fix dummy file PointSET.java

--- kdtree/src/PointSET.java
+++ kdtree/src/PointSET.java
@@ -6,5 +6,34 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.TreeSet;
 public class PointSET {
     private TreeSet<Point2D> points;
-    
+    public PointSET() {
+        points = new TreeSet<>();
+    }
+    public boolean isEmpty() {
+        return points.isEmpty();
+    }
+    public int size() {
+        return points.size();
+    }
+    public void insert(Point2D p) {
+        points.add(p);
+    }
+    public boolean contains(Point2D p) {
+        return points.contains(p);
+    }
+    public void draw() {
+        for (Point2D p : points) {
+            p.draw();
+        }
+    }
+    public Iterable<Point2D> range(RectHV rectHV) {
+        return null;
+    }
+    public  Point2D nearest(Point2D p) {
+        return null;
+    }
+    public static void main(String[] args) {
+        PointSET s = new PointSET();
+        System.out.println("Hello");
+    }
 }
